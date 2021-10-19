686f1f51a7b3314e408ae8e20aed84095b61c005
Daniel Nunes <danielzcomputacao@gmail.com>
2021-10-19 16:41:22 -0300

    back end

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
+
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
 
 app.listen(4000, () => console.log("🚀 Server is running on PORT 4000"));
