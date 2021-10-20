e3492fe88e1e6db66f5422524c91213bf69750f5
Daniel Nunes <danielzcomputacao@gmail.com>
2021-10-20 10:04:54 -0300

    getting last 3 messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -12,5 +13,7 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+
+router.get("/messages/last3", new GetLast3MessagesController().handle);
 
 export { router };
