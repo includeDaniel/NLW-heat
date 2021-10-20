e996d6514af9f61bd597f6f238331554f5088135
Daniel Nunes <danielzcomputacao@gmail.com>
2021-10-19 23:07:00 -0300

    middleware

--- src/middleware/ensureAuthenticated.ts
+++ src/middleware/ensureAuthenticated.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
+interface IPayload {
+  sub: string;
+}
+
 export function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -15,5 +19,13 @@ export function ensureAuthenticated(
   }
 
   const [, token] = authToken.split("");
-  const { sub } = verify(token, process.env.JWT_SECRET);
+
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+
+    request.user_id = sub;
+    return next();
+  } catch (err) {
+    return response.status(401).json({ errorCode: "token.expired" });
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post(
+  "/messages",
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
 
 export { router };
