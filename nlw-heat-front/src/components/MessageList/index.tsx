bcd885ac3c215a1d6e512ff416bdc7487fd1a0bf
Daniel Nunes <danielzcomputacao@gmail.com>
2021-10-21 15:31:30 -0300

    conection with api

--- nlw-heat-front/src/components/MessageList/index.tsx
+++ nlw-heat-front/src/components/MessageList/index.tsx
@@ -1,62 +1,43 @@
+import { api } from "../../services/api";
+
 import styles from "./style.module.scss";
 import logoImg from "../../assets/logo.svg";
+import { useEffect, useState } from "react";
 
 export function MessageList() {
+  type Message = {
+    id: string;
+    text: string;
+    user: {
+      name: string;
+      avatar_url: string;
+    };
+  };
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            {" "}
-            Não vejo a hora de começar o evento, com certeza vai ser o melhor de
-            todos os tempos Vamooo pra cima!🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/includedaniel.png"
-                alt="Daniel Nunes"
-              />
-            </div>
-            <span>Daniel Nunes</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            {" "}
-            Não vejo a hora de começar o evento, com certeza vai ser o melhor de
-            todos os tempos Vamooo pra cima!🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/includedaniel.png"
-                alt="Daniel Nunes"
-              />
-            </div>
-            <span>Daniel Nunes</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            {" "}
-            Não vejo a hora de começar o evento, com certeza vai ser o melhor de
-            todos os tempos Vamooo pra cima!🔥🔥
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/includedaniel.png"
-                alt="Daniel Nunes"
-              />
-            </div>
-
-            <span>Daniel Nunes</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
