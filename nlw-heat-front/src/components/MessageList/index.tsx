adb65db7a00b3c188a3ffa29061c5bf580bc4d4c
Daniel Nunes <danielzcomputacao@gmail.com>
2021-10-21 18:15:54 -0300

    messages in real time

--- nlw-heat-front/src/components/MessageList/index.tsx
+++ nlw-heat-front/src/components/MessageList/index.tsx
@@ -2,18 +2,40 @@ import { api } from "../../services/api";
 
 import styles from "./style.module.scss";
 import logoImg from "../../assets/logo.svg";
+
 import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
+const messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
 
 export function MessageList() {
-  type Message = {
-    id: string;
-    text: string;
-    user: {
-      name: string;
-      avatar_url: string;
-    };
-  };
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>("messages/last3").then((response) => {
