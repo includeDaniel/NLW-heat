e498c5c8015e4d85aa6a06195d62133d5a607391
Daniel Nunes <danielzcomputacao@gmail.com>
2021-10-21 15:37:48 -0300

    github button

--- nlw-heat-front/src/components/LoginBox/index.tsx
+++ nlw-heat-front/src/components/LoginBox/index.tsx
@@ -3,10 +3,12 @@ import { VscGithubInverted } from "react-icons/vsc";
 import styles from "./styles.module.scss";
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c14166ed6be4eeba233e`;
+
   return (
     <div className={styles.LoginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
